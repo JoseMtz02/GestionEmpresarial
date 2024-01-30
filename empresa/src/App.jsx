4256381763fe0a5b9fd8116b7d879c1031e3f29e
JoseMtz02 <josemtzlara@hotmail.com>
2024-01-30 12:12:30 -0500

    cambios de rama

--- empresa/src/App.jsx
+++ empresa/src/App.jsx
@@ -2,12 +2,9 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Asignacion from './views/Asignacion';
-<<<<<<< HEAD
 import Equipos from './views/Equipos';
 import Proyectos from './views/Proyectos';
 import Contactanos from './views/Contactanos';
-=======
->>>>>>> ff9a50cae9793aa445f38b0c846474e4e7235790
 
 function App() {
 
@@ -15,13 +12,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Asignacion/>}></Route>
-<<<<<<< HEAD
         <Route path='/Equipos' element={<Equipos/>}></Route>
         <Route path='/Proyectos' element={<Proyectos/>}></Route>
         <Route path='/Contactanos' element={<Contactanos/>}></Route>
        
-=======
->>>>>>> ff9a50cae9793aa445f38b0c846474e4e7235790
       </Routes>
     </BrowserRouter>
   )
