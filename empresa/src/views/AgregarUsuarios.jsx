66c2e66fa39284a7f1861450045166534abf4ec9
JoseMtz02 <josemtzlara@hotmail.com>
2024-01-31 12:04:22 -0500

    actualiza rama

---
+++ empresa/src/views/AgregarUsuarios.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AgregarUsuarios(){
+    return (
+        <>
+        <Header> </Header>  
+        
+        </>
+    );
+};
+export default AgregarUsuarios;
